c68048770f4059063735a632a25655de9cd00c8e
dzee22 <94622233+dzee22@users.noreply.github.com>
2021-11-20 21:27:21 +0700

    Update n-tagowner.js

--- plugins/n-tagowner.js
+++ plugins/n-tagowner.js
@@ -2,11 +2,11 @@ let handler = async (m, { conn, text }) => {
     let name = m.fromMe ? conn.user : conn.contacts[m.sender]
 
   conn.reply(m.chat, `
-Sayanggg, Ada Yang Manggil Tuhhh😊
+Apa tag tag owner?
 `.trim(), m)
     let mentionedJid = [m.sender]
 }
-handler.customPrefix = /@6285346545126/i
+handler.customPrefix = /@6282146176911|6289621278920/i
 handler.command = new RegExp
 
 module.exports = handler
