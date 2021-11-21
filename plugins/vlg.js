922a534db4f9bce675e3b804abd42a3821f7a0c7
dzee22 <94622233+dzee22@users.noreply.github.com>
2021-11-21 08:31:44 +0700

    Update and rename pubg.js to vlg.js

---
+++ plugins/vlg.js
@@ -0,0 +1,51 @@
+let handler = async m => m.reply(`
+*LIST HARGA FREE FIRE VIA LOGIN*
+*_RATE 12,4_*
+
+*Log Google Minimal 5$*
+**Log Fb / vk minim 2$*
+*DATA KE [ 8114 ]*
+**WAJIB A2FON + KODE PEMULIHAN*
+*GAK BAWA KODE? GK PROSES!!!*
+200 💎 (2$) = 24.800
+310 💎 (3$) = 37.200
+410 💎 (4$) = 49.600
+520 💎 (5$) = 62.000
+620 💎 (6$) = 74.400
+720 💎 (7$) = 86.800
+830 💎 (8$) = 99.200
+930 💎 (9$) = 111.600
+1060 💎 (10$) = 124.000
+1370 💎  (13$) = 161.200
+1580 💎 (15$) = 186.000
+2180 💎 (20$) = 248.000
+3010 💎 (28$) = 347.200
+3240 💎 (30$) = 372.000
+3760 💎 (35$) = 434.000
+4360 💎 (40$) = 496.000
+5600 💎 (50$) = 620.000
+11.200 💎 (100$) = 1.240.000
+
+Mm (2$) : 24.800
+Mb (8$)  : 99.200
+
+___________________________
+
+
+*LIST VILOG PUBGM*
+325 UC (5$) = 62.000
+650 UC (10$) = 124.000
+1800 UC (25$) = 310.000
+8100 UC (100$) = 1.240.000
+
+Nomor/Email        Sandi       Kode Pemulihan
+
+*NB = AMAN KAN AKUN SETELAH DONE BIAR GAK DRAMA*
+`.trim()) // Tambah sendiri kalo mau
+handler.help = ['vlg']
+handler.tags = ['store']
+handler.command = /^(vlg)$/i
+
+handler.limit = false
+
+module.exports = handler
