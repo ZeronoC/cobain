14558b8e8f33c543a809d76c46c4a61148880a8a
dzee22 <94622233+dzee22@users.noreply.github.com>
2021-11-21 08:33:46 +0700

    Create pubg.js

---
+++ plugins/pubg.js
@@ -0,0 +1,25 @@
+let handler = async m => m.reply(`
+*LIST HARGA PUBG VIA ID*
+*_RATE 0.89_
+
+50+2 UC = 8.900
+60+2 UC = 10.680
+120+2 UC = 21.360
+150+6 UC = 26.700
+180+6 UC = 32.040
+250+13 UC = 44.500
+500+25 UC = 89.000
+1250+125 = 222.500
+2500+375 = 445.000
+
+
+*PROSES 1-30 MENIT*
+❗SERTAKAN BUKTI TRANSFER
+`.trim()) // Tambah sendiri kalo mau
+handler.help = ['pubg']
+handler.tags = ['store']
+handler.command = /^(pubg)$/i
+
+handler.limit = false
+
+module.exports = handler
