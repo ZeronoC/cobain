1cc17e774d127201a46f01b74142dd641986bec0
dzee22 <94622233+dzee22@users.noreply.github.com>
2021-11-21 17:17:49 +0700

    Create payment.js

---
+++ plugins/payment.js
@@ -0,0 +1,27 @@
+let handler = async m => m.reply(`
+*P α y m e n t*
+💸 GOPAY : scan pp a/n indra 
+
+💸 DANA : 082123021898 an indra 
+
+💸 SHOPEEPAY : 089653328114 
+
+💸 OVO : 082119829712 AN LYRAA
+
+💸 BANK? BRI ( tnya admin ) 
+
+💸 ALFAMART / INDOMART ( Minim Trx 20k ) 
+
+⿴⃟۪۪⃕᎒⃟📎 *Sertακαn Bυκti SS!!*
+⿴⃟۪۪⃕᎒⃟📎 *Salah TF No reff!*
+⿴⃟۪۪⃕᎒⃟📎 *Nb : Kurang lengkapi, Lebih anggap sedekah:v*
+
+*HARAP BACA SALAH TF JANGAN SALAHIN ADMIN!!*
+`.trim()) // Tambah sendiri kalo mau
+handler.help = ['payment']
+handler.tags = ['store']
+handler.command = /^(payment)$/i
+
+handler.limit = false
+
+module.exports = handler
