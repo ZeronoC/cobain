9b66c037704a88db112e9e4e7e3635157b2bdec7
dzee22 <94622233+dzee22@users.noreply.github.com>
2021-11-21 08:36:20 +0700

    Create gs.js

---
+++ plugins/gs.js
@@ -0,0 +1,19 @@
+let handler = async m => m.reply(`
+*TANYAKAN STOK!!*
+**HARGA RANDOM*
+
+
+*˖˚𖠧 33 GS 8.600 - 9.000*
+* *˖˚𖠧 66 GS 17.000 - 18.000*
+*˖˚𖠧 165 GS 43.000 - 45.000*
+* *˖˚𖠧 330 GS 86.000 - 90.000*
+
+*❥• PROSES FAST 1-10 MENIT*
+`.trim()) // Tambah sendiri kalo mau
+handler.help = ['gs']
+handler.tags = ['store']
+handler.command = /^(gs)$/i
+
+handler.limit = false
+
+module.exports = handler
