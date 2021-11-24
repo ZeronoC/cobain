d220b8ae8023999d5a95b2b36cdf8095022c4e33
dzee22 <94622233+dzee22@users.noreply.github.com>
2021-11-24 15:16:39 +0700

    Create o-getplugin.js

---
+++ plugins/o-getplugin.js
@@ -0,0 +1,24 @@
+let cp = require('child_process')
+let { promisify } = require('util')
+let exec = promisify(cp.exec).bind(cp)
+
+let handler = async (m, { conn, isROwner, command, text }) => {
+    if (!isROwner) return
+    if (global.conn.user.jid != conn.user.jid) return
+    m.reply('Executing...')
+    let o
+    try {
+        o = await exec('cat plugins/' + text + '.js')
+    } catch (e) {
+        o = e
+    } finally {
+        let { stdout, stderr } = o
+        if (stdout.trim()) m.reply(stdout)
+        if (stderr.trim()) m.reply(stderr)
+    }
+}
+handler.help = ['getplugin'].map(v => v + ' <text>')
+handler.tags = ['owner']
+handler.command = /^(getplugin)$/i
+
+module.exports = handler
