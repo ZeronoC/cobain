e2ac53317efec14b382e738f660cc1be967fbac6
dzee22 <94622233+dzee22@users.noreply.github.com>
2021-11-24 07:44:18 +0700

    Update sewardp.js

--- plugins/sewardp.js
+++ plugins/sewardp.js
@@ -7,9 +7,9 @@ Pembayaran:
 Teruskan pesan ini kepada owner untuk segera di selesaikan.
 NB: *Sertakan bukti transfer*
 `.trim()) // Tambah sendiri kalo mau
-handler.help = ['buyrdp']
+handler.help = ['sewardp']
 handler.tags = ['store']
-handler.command = /^(buyrdp)$/i
+handler.command = /^(sewardp)$/i
 
 handler.limit = false
 
