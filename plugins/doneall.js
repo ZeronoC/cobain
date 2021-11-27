cde2775ee12aeb128d0661bf92fa9f2f9cc3bbfb
dzee22 <94622233+dzee22@users.noreply.github.com>
2021-11-27 13:38:46 +0700

    Create doneall.js

---
+++ plugins/doneall.js
@@ -0,0 +1,17 @@
+let handler = async m => m.reply(`
+*STATUS PESANAN*
+
+*DONE ALL*
+
+*TERIMAKASIH ATAS KUNJUNGANNYA *
+
+*© Lyra*
+`.trim()) // Tambah sendiri kalo mau
+handler.help = ['doneall']
+handler.tags = ['store']
+handler.customPrefix = /^doneall$/i
+handler.command = new RegExp
+
+handler.limit = false
+
+module.exports = handler
