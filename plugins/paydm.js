925476b1a31380bfd1ee39b5e6ad64be56489200
dzee22 <94622233+dzee22@users.noreply.github.com>
2021-11-27 12:24:59 +0700

    Add files via upload

---
+++ plugins/paydm.js
@@ -0,0 +1,31 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => await conn.sendButtonImg(m.chat, await (await fetch('https://telegra.ph/file/665046853bce1de9d2d5d.jpg')).buffer(), `
+*P α y m e n t*
+
+❀ GOPAY : scan qr a/n Y S H
+
+❀ DANA : 089653328114 an indra 
+
+❀ SHOPEEPAY : 089653328114 
+
+❀ OVO : 082119829712 AN LYRAA
+
+❀ BANK? BRI ( tnya admin ) 
+
+❀ ALFAMART / INDOMART ( Minim Trx 20k ) 
+
+*Sertακαn Bυκti SS!!*
+*Salah TF No reff!*
+*Nb : Kurang lengkapi, Lebih anggap sedekah:v*
+
+*HARAP BACA SALAH TF JANGAN SALAHIN ADMIN!!*
+
+
+`.trim(), 'ini weem yg buat @rasel.ganz jangan di hapus lah banh:(', 'Ini Button banh', '@rasel.ganz', m)
+
+handler.help = ['pay']
+handler.tags = ['store']
+handler.customPrefix = /^pay$/i
+handler.command = new RegExp
+
+module.exports = handler
