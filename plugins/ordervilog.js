1efcfb8c33b6ad74355c02d14b2391fb3d994f26
dzee22 <94622233+dzee22@users.noreply.github.com>
2021-11-30 12:46:00 +0700

    Create ordervilog.js

---
+++ plugins/ordervilog.js
@@ -0,0 +1,21 @@
+let handler = async m => m.reply(`
+Isi data dibawah 
+
+WAJIB DIISI BIAR TIDAK SALAH KIRIM DMNYA
+
+LOGIN FB / GOOGLE :
+EMAIL :
+PW :
+KODE :
+ORDER :
+
+Nb : kirim di pc [8114]
+`.trim()) // Tambah sendiri kalo mau
+handler.help = ['ordervilog']
+handler.tags = ['store']
+handler.customPrefix = /^order vilog$/i
+handler.command = new RegExp
+
+handler.limit = false
+
+module.exports = handler
