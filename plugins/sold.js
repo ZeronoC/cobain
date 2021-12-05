e9999ee2ea28d453ff88bc12aeb04c5797fe145e
dzee22 <94622233+dzee22@users.noreply.github.com>
2021-12-05 08:27:47 +0700

    Create sold.js

---
+++ plugins/sold.js
@@ -0,0 +1,11 @@
+let handler = async m => m.reply(`
+*SOLD YA SAYANG, LAIN KALI FAST RESPON*
+`.trim()) // Tambah sendiri kalo mau
+handler.help = ['gcstore']
+handler.tags = ['store']
+handler.customPrefix = /^sold$/i
+handler.command = new RegExp
+
+handler.limit = false
+
+module.exports = handler
