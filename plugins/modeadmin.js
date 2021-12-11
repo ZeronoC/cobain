f2189efe7381a10df1ffa63e3388deaf29c59096
dzee22 <94622233+dzee22@users.noreply.github.com>
2021-12-11 10:13:39 +0700

    Update modeadmin.js

--- plugins/modeadmin.js
+++ plugins/modeadmin.js
@@ -1,21 +1,21 @@
 let handler  = async (m, { conn, args }) => {
 	if(!args || !args[0]) {
 		await conn.updatePresence(m.chat, Presence.composing) 
-		conn.reply(m.chat, `*ADMIN MODE*\n\nBerikan keterangan on atau off !`, m)
+		conn.reply(m.chat, `*MODE ADMIN*\n\nBerikan keterangan on atau off !`, m)
 	} else if(args[0] == 'on') {
     global.DATABASE.data.chats[m.chat].adminMode = true
-		conn.reply(m.chat, `*ADMIN MODE*\n\nBot berhasil di ubah menjadi Admin Mode\n\nHanya admin yang dapat menggunakan bot di grup ini`, m)
+		conn.reply(m.chat, `*MODE ADMIN*\n\nBot berhasil di ubah menjadi Admin Mode\n\nHanya admin yang dapat menggunakan bot di grup ini`, m)
 	} else if(args[0] == 'off') {
     global.DATABASE.data.chats[m.chat].adminMode = false
-		conn.reply(m.chat, `*ADMIN MODE*\n\nBerhasil menonaktifkan Admin Mode\n\nSemua anggota grup bisa menggunakan bot di grup ini`, m)
+		conn.reply(m.chat, `*MODE ADMIN*\n\nBerhasil menonaktifkan Admin Mode\n\nSemua anggota grup bisa menggunakan bot di grup ini`, m)
 	} else {
 		await conn.updatePresence(m.chat, Presence.composing) 
-		conn.reply(m.chat, `*ADMIN MODE*\n\nBerikan keterangan on atau off !`, m)
+		conn.reply(m.chat, `*MODE ADMIN*\n\nBerikan keterangan on atau off !`, m)
 	} 
 }
-handler.help = ['adminmode *on/off*']
-handler.tags = ['group admin']
-handler.command = /^(adminmode)$/i
+handler.help = ['modeadmin *on/off*']
+handler.tags = ['group']
+handler.command = /^(modeadmin)$/i
 handler.admin = true
 handler.group = true
 handler.botAdmin = true
