f27de9afaaf9e597a885d594e4218377a2092137
dzee22 <94622233+dzee22@users.noreply.github.com>
2021-12-18 17:44:32 +0700

    Update payment.js

--- plugins/payment.js
+++ plugins/payment.js
@@ -1,5 +1,5 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn }) => await conn.sendImg(m.chat, await (await fetch('https://telegra.ph/file/fcc6abc1a05e9e5d5c6c7.jpg')).buffer(), `
+let handler = async (m, { conn }) => await conn.sendButtonImg(m.chat, await (await fetch('https://telegra.ph/file/fcc6abc1a05e9e5d5c6c7.jpg')).buffer(), `
 *P α y m e n t*
 
 ❀ GOPAY : scan qr a/n Y S H
@@ -21,7 +21,7 @@ let handler = async (m, { conn }) => await conn.sendImg(m.chat, await (await fet
 *HARAP BACA SALAH TF JANGAN SALAHIN ADMIN!!*
 
 
-`.trim())
+`.trim(), watermark, 'Gausah Di Pencet', '', m)
 
 handler.help = ['pay']
 handler.tags = ['store']
